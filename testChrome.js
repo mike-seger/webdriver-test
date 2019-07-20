f70c20b435409f096556c506a1ef467b6dd99878
michael <michael@n.c>
2019-07-20 19:32:46 +0000

    ...

--- testChrome.js
+++ testChrome.js
@@ -1,9 +1,68 @@
-from selenium import webdriver
+import * as driver from 'webdriverio';
+import assert from 'assert';
+import http from 'superagent';
+import superagentPromisePlugin from 'superagent-promise-plugin';
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-d = webdriver.Chrome('/home/michael/git/webdriver-test/chromedriver/chromedriver',
-    chrome_options=chrome_options)
-d.get('https://www.google.com/')
+
+describe('Can mock api responses', function () {
+    let client;
+    before(function () {
+        this.timeout(10000);
+        client = driver.remote(
+        {
+            desiredCapabilities: {
+                'browserName': 'chrome',
+                'chromeOptions': {
+                    'binary': "/home/michael/git/webdriver-test/chromedriver/chromedriver",
+                    "args": ['--headless', '--disable-gpu', '--window-size=1280,800']
+                }
+            }
+        }
+        );
+        return client.init();
+    });
+
+    it('should return body of "joe bloggs" when accessing the server', () =>
+        client
+            .url('http://localhost:3000')
+            .getText('body')
+            .then(body => {
+                assert(body === 'joe bloggs',
+                    body + ' does not equal joe bloggs');
+            })
+    );
+
+    it('should return body of "jane doe" when pointing at the mock', () =>
+        http
+            .post('http://localhost:8080/__admin/mappings/new')
+            .use(superagentPromisePlugin)
+            .send({
+                request: {
+                    url: "/api/user",
+                    method: "GET"
+                },
+                response: {
+                    status: 200,
+                    "headers": {
+                        "access-control-allow-origin": "*",
+                        "content-type": "application/json"
+                    },
+                    "jsonBody": {
+                        "firstName": "jane",
+                        "surname": "doe"
+                    }
+                }
+            })
+            .then(() =>
+                client
+                    .url('http://localhost:3000/mocked')
+                    .getText('body')
+                    .then(body => {
+                        assert(body === 'jane doe',
+                            body + ' does not equal jane doe');
+                    }))
+    );
+
+    after(() => client.end());
+});
+
--- testChrome.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.binary_location = "C:\\path\\to\\chrome.exe"
-driver = webdriver.Chrome(chrome_options = options, executable_path=r'C:\path\to\chromedriver.exe')
-driver.get('http://google.com/')
-print("Chrome Browser Invoked")
-driver.quit()
